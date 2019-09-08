9ff7b3efd4275cb718e31c15c5502777eb99a83a
cowarder <1615482151@qq.com>
2019-09-08 14:45:17 +0800

    change directory name

--- chapter2-Core-Operations/arithmetic-operation.py
+++ chapter2-Core-Operations/arithmetic-operation.py
@@ -12,10 +12,24 @@ print(x + y)                                        # [4] numpy would get mod
 
 img1 = cv2.imread('../pics/forest.jpeg', 1)
 img2 = cv2.imread('../pics/lenna.jpeg', 1)
-print(img1.size())
-print(img2.size())
+w = int(img1.shape[1])
+h = int(img1.shape[0])
+dim = (w, h)
 
-dst = cv2.addWeighted(img1, 0.3, img2, 0.7, 0)
+
+"""
+    interpolations:
+    1.cv.INTER_NEAREST:
+    2.cv2.INTER_LINEAR: 
+    3.cv2.INTER_CUBIC: 
+    4.cv2.INTER_AREA:
+"""
+img2 = cv2.resize(img2, dim, interpolation=cv2.INTER_AREA)
+
+print(img1.shape)
+print(img2.shape)
+
+dst = cv2.addWeighted(img1, 0.5, img2, 0.5, 0)
 
 cv2.imshow('dst', dst)
 cv2.waitKey(0)
